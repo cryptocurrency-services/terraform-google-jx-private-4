d52a0137d43166b03578e7616d63017df64dfd67
kevinstl <kjwilde@gmail.com>
2022-06-03 18:05:04 -0500

    Update to new versions

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@
 // ----------------------------------------------------------------------------
 terraform {
 //  required_version = ">= 0.12.0, < 0.15"
-  required_version = ">= 0.12.0, < 1.1.0"
+  required_version = ">= 0.12.0, < 2.2.0"
 }
 
 // ----------------------------------------------------------------------------
@@ -13,12 +13,12 @@ terraform {
 // ----------------------------------------------------------------------------
 provider "google" {
   project = var.gcp_project
-  version = ">= 3.46.0"
+  version = ">= 3.46.0, < 4.0.0"
 }
 
 provider "google-beta" {
   project = var.gcp_project
-  version = ">= 3.46.0"
+  version = ">= 3.46.0, < 4.0.0"
 }
 
 provider "random" {
